ea62ab5d06869cac03584d80be56c042f33dfaca
beth-ross <beth_ross@fws.gov>
2022-09-21 11:29:00 -0600

    Geo mean function and fig

--- Analysis/KRST_figs.R
+++ Analysis/KRST_figs.R
@@ -3,14 +3,21 @@ library(coda)
 library(vioplot)
 library(gplots)
 
-#load("~/KRST/Analysis/results_currentnm_5852.RData")
+load("~/KRST/Analysis/results_currentnm_5852.RData")
 load("~/KRST/Analysis/results_currentnm_2455.RData")
+load("~/KRST/Analysis/results_lowis_2455.RData")
+load("~/KRST/Analysis/results_noincu_2455.RData")
+load("~/KRST/Analysis/results_currentnm_2455_is1.RData")
+load("~/KRST/Analysis/results_currentnm_2455_is0.RData")
 
 out.2455 = as.mcmc(rbind(out.current.2455[[1]], out.current.2455[[2]], out.current.2455[[3]]))
 out.5852 = as.mcmc(rbind(out.current.5852[[1]], out.current.5852[[2]], out.current.5852[[3]]))
 
 out.is0 = as.mcmc(rbind(out.current.2455.is0[[1]], out.current.2455.is0[[2]], out.current.2455.is0[[3]]))
 out.is1 =as.mcmc(rbind(out.current.2455.is1[[1]], out.current.2455.is1[[2]], out.current.2455.is1[[3]]))
+
+out.lis =as.mcmc(rbind(out.lowis.2455[[1]], out.lowis.2455[[2]], out.lowis.2455[[3]]))
+out.noincu =as.mcmc(rbind(out.noincu.2455[[1]], out.noincu.2455[[2]], out.noincu.2455[[3]]))
 
 col.plot=viridis(5,alpha=.6)
 
@@ -129,19 +136,26 @@ Nb.high.is0 = apply(out.is0[,1:(Time+Time.fore)],2,quantile,0.975)
 Nb.is1 = apply(out.is1[,1:(Time+Time.fore)],2,mean)
 Nb.low.is1 = apply(out.is1[,1:(Time+Time.fore)],2,quantile,0.025)
 Nb.high.is1 = apply(out.is1[,1:(Time+Time.fore)],2,quantile,0.975)
+Nb.lis = apply(out.lis[,1:(Time+Time.fore)],2,mean)
+Nb.low.lis = apply(out.lis[,1:(Time+Time.fore)],2,quantile,0.025)
+Nb.high.lis = apply(out.lis[,1:(Time+Time.fore)],2,quantile,0.975)
+Nb.noincu = apply(out.noincu[,1:(Time+Time.fore)],2,mean)
+Nb.low.noincu = apply(out.noincu[,1:(Time+Time.fore)],2,quantile,0.025)
+Nb.high.noincu = apply(out.noincu[,1:(Time+Time.fore)],2,quantile,0.975)
+
 
 plot(Nb.2455, x= seq(1994,plot.year),type="l",
      ylim=c(0,max(Nb.high.2455)),
      xlab = "Year", ylab = "Abundance")
 polygon(c(seq(1994,plot.year),rev(seq(1994,plot.year))),
         c(Nb.low.2455,rev(Nb.high.2455)),col=col.plot[3])
-lines(Nb.5852, x = seq(1994,plot.year), type="l", ylab="Abundance",
-      xlab="Year",ylim=c(0,max(imm.h)))
+lines(Nb.5852, x = seq(1994,plot.year), lty=3, ylab="Abundance",
+      xlab="Year",ylim=c(0,max(Nb.high.2455)),lwd=2)
 polygon(c(seq(1994,plot.year), rev(seq(1994,plot.year))),
         c(Nb.low.5852,rev(Nb.high.5852)),col=col.plot[5])
 
 legend("topleft", 
-       legend=c("SSP2-4.5, 2 m SLR", "SSP5-8.5, 2 m SLR"), pch=c(16,16),
+       legend=c("SSP2-4.5, 0.5 m SLR", "SSP5-8.5, 2 m SLR"), pch=c(16,16),
        bty="n",col=c(col.plot[3],col.plot[5]),
        x.intersp=0.7, y.intersp=1.0, cex=1.1, pt.cex=1.5)
 
@@ -154,6 +168,28 @@ legend("topleft",
        bty="n",col = c(col.plot[3],col.plot[5]),
        x.intersp=0.7, y.intersp=1.0, cex=1.1, pt.cex=1.5)
 
+#PAIS low in situ
+lines(Nb.lis, x = seq(1994,plot.year), lty=3, ylab="Abundance",
+      xlab="Year",ylim=c(0,max(Nb.2455)),lwd=2)
+polygon(c(seq(1994,plot.year), rev(seq(1994,plot.year))),
+        c(Nb.low.lis,rev(Nb.high.lis)),col=col.plot[5])
+
+legend("topleft", 
+       legend=c("Current Management", "8% In Situ PAIS"), pch=c(16,16),
+       bty="n",col = c(col.plot[3],col.plot[5]),
+       x.intersp=0.7, y.intersp=1.0, cex=1.1, pt.cex=1.5)
+
+#no incubation at PAIS
+lines(Nb.noincu, x = seq(1994,plot.year), lty=3, ylab="Abundance",
+      xlab="Year",ylim=c(0,max(Nb.2455)),lwd=2)
+polygon(c(seq(1994,plot.year), rev(seq(1994,plot.year))),
+        c(Nb.low.noincu,rev(Nb.high.noincu)),col=col.plot[5])
+
+legend("topleft", 
+       legend=c("Current Management", "No Incubation"), pch=c(16,16),
+       bty="n",col = c(col.plot[3],col.plot[5]),
+       x.intersp=0.7, y.intersp=1.0, cex=1.1, pt.cex=1.5)
+
 
 #### LAMBDA ####
 lam = matrix(NA,30000,(Time+Time.fore-1))
@@ -162,9 +198,12 @@ lam.b.2455 = matrix(NA,30000,(Time+Time.fore-1))
 lam.b.5852 = matrix(NA,30000,(Time+Time.fore-1))
 lam.b.is0 = matrix(NA,30000, (Time+Time.fore-1))
 lam.b.is1 = matrix(NA,30000, (Time+Time.fore-1))
-for(t in 1:(Time+Time.fore)){
-  n.tot[,t] = rowSums(cbind(out[,t], out[,(2*(Time+Time.fore))+t]))
-}
+lam.b.lis = matrix(NA,30000, (Time+Time.fore-1))
+lam.b.noin = matrix(NA,30000, (Time+Time.fore-1))
+
+#for(t in 1:(Time+Time.fore)){
+#  n.tot[,t] = rowSums(cbind(out[,t], out[,(2*(Time+Time.fore))+t]))
+#}
 
 for(t in 1:(Time+Time.fore-1)){ 
   #lam[,t] = n.tot[,t+1]/n.tot[,t]
@@ -172,12 +211,47 @@ for(t in 1:(Time+Time.fore-1)){
   lam.b.5852[,t] = out.5852[,t+1]/out.5852[,t]
   lam.b.is0[,t] = out.is0[,t+1]/out.is0[,t]
   lam.b.is1[,t] = out.is1[,t+1]/out.is1[,t]
+  lam.b.lis[,t] = out.lis[,t+1]/out.lis[,t]
+  lam.b.noin[,t] = out.noincu[,t+1]/out.noincu[,t]
   }
 #remove Inf from lam
 lam.b.2455[!is.finite(lam.b.2455)] = NA
 lam.b.5852[!is.finite(lam.b.5852)] = NA
 lam.b.is0[!is.finite(lam.b.is0)] = NA
 lam.b.is1[!is.finite(lam.b.is1)]= NA
+lam.b.lis[!is.finite(lam.b.lis)] = NA
+lam.b.noin[!is.finite(lam.b.noin)] = NA
+
+#geo mean of lambda through future
+geo_mean <- function(data){
+  gm = rep(NA,30000)
+  for(i in 1:length(data[,1])){
+  gm[i] <- exp(mean(log(data[i,])))
+  }
+  return(gm)
+  }
+
+gm.lam.2455 = geo_mean(lam.b.2455[,29:72])
+gm.lam.5852 = geo_mean(lam.b.5852[,29:72])
+gm.lam.is1 = geo_mean(lam.b.is1[,29:72])
+gm.lam.is0 = geo_mean(lam.b.is0[,29:72])
+gm.lam.lis = geo_mean(lam.b.lis[,29:72])
+gm.lam.noin = geo_mean(lam.b.noin[,29:72])
+
+boxplot(cbind(gm.lam.2455,gm.lam.5852,gm.lam.is1,
+              gm.lam.is0,gm.lam.lis,gm.lam.noin),
+        ylab = expression(lambda),
+        names = c("Current,SSP2","Current,SSP5",
+                  "All In Situ -1","All In Situ 0",
+                  "Limited In Situ","No Incubation"),outline=FALSE)
+abline(h=1)
+
+zero.gm.2455 = length(which(gm.lam.2455==0))
+zero.gm.5852 = length(which(gm.lam.5852==0))
+zero.gm.is1 = length(which(gm.lam.is1==0))
+zero.gm.is0 = which(gm.lam.is0==0)
+zero.gm.lis = which(gm.lam.lis==0)
+zero.gm.noin = which(gm.lam.noin==0)
 
 lam.low = apply(lam[,1:(Time-1)],2,quantile,0.025,na.rm=TRUE)
 lam.high = apply(lam[,1:(Time-1)],2,quantile,0.975,na.rm=TRUE)
@@ -185,16 +259,22 @@ lam.low.fore.2455 = apply(lam.b.2455[,(Time):(Time+Time.fore-1)],2,quantile,0.02
 lam.low.fore.5852 = apply(lam.b.5852[,(Time):(Time+Time.fore-1)],2,quantile,0.025,na.rm=TRUE)
 lam.low.fore.is0 = apply(lam.b.is0[,(Time):(Time+Time.fore-1)],2,quantile,0.025,na.rm=TRUE)
 lam.low.fore.is1 = apply(lam.b.is1[,(Time):(Time+Time.fore-1)],2,quantile,0.025,na.rm=TRUE)
+lam.low.fore.lis = apply(lam.b.lis[,(Time):(Time+Time.fore-1)],2,quantile,0.025,na.rm=TRUE)
+lam.low.fore.noin = apply(lam.b.noin[,(Time):(Time+Time.fore-1)],2,quantile,0.025,na.rm=TRUE)
 
 lam.high.fore.2455 = apply(lam.b.2455[,(Time):(Time+Time.fore-1)],2,quantile,0.975)
 lam.high.fore.5852 = apply(lam.b.5852[,(Time):(Time+Time.fore-1)],2,quantile,0.975)
 lam.high.fore.is0 = apply(lam.b.is0[,(Time):(Time+Time.fore-1)],2,quantile,0.975,na.rm=TRUE)
 lam.high.fore.is1 = apply(lam.b.is1[,(Time):(Time+Time.fore-1)],2,quantile,0.975,na.rm=TRUE)
+lam.high.fore.lis = apply(lam.b.lis[,(Time):(Time+Time.fore-1)],2,quantile,0.975,na.rm=TRUE)
+lam.high.fore.noin = apply(lam.b.noin[,(Time):(Time+Time.fore-1)],2,quantile,0.975,na.rm=TRUE)
 
 lam.med.all.2455 = apply(lam.b.2455[,1:(Time+Time.fore-1)],2,quantile,0.5,na.rm=TRUE)
 lam.med.all.5852 = apply(lam.b.5852[,1:(Time+Time.fore-1)],2,quantile,0.5,na.rm=TRUE)
 lam.med.all.is0 = apply(lam.b.is0[,1:(Time+Time.fore-1)],2,quantile,0.5,na.rm=TRUE)
 lam.med.all.is1 = apply(lam.b.is1[,1:(Time+Time.fore-1)],2,quantile,0.5,na.rm=TRUE)
+lam.med.all.lis = apply(lam.b.lis[,1:(Time+Time.fore-1)],2,quantile,0.5,na.rm=TRUE)
+lam.med.all.noin = apply(lam.b.noin[,1:(Time+Time.fore-1)],2,quantile,0.5,na.rm=TRUE)
 
 par(mfrow=c(1,1))
 #vioplot(lam[,-1],ylim=c(0,3),names=seq(1993,(1991+Time+Time.fore)))
@@ -229,6 +309,16 @@ polygon(c(seq(Time,(Time+Time.fore-1)),
           rev(seq(Time,(Time+Time.fore-1)))),
         c(lam.low.fore.is1,rev(lam.high.fore.is1)),
         col=col.plot[5])
+#low in situ, 8% at PAIS
+polygon(c(seq(Time,(Time+Time.fore-1)), 
+          rev(seq(Time,(Time+Time.fore-1)))),
+        c(lam.low.fore.lis,rev(lam.high.fore.lis)),
+        col=col.plot[5])
+#no incubation at PAIS
+polygon(c(seq(Time,(Time+Time.fore-1)), 
+          rev(seq(Time,(Time+Time.fore-1)))),
+        c(lam.low.fore.noin,rev(lam.high.fore.noin)),
+        col=col.plot[5])
 
 lines(y=lam.med.all.5852[Time:(Time+Time.fore-1)],
       x=seq(Time,(Time+Time.fore-1)),lty=3,lwd=2)
@@ -238,11 +328,16 @@ lines(y=lam.med.all.is0[Time:(Time+Time.fore-1)],
       x=seq(Time,(Time+Time.fore-1)),lty=1,lwd=2)
 lines(y=lam.med.all.is1[Time:(Time+Time.fore-1)],
       x=seq(Time,(Time+Time.fore-1)),lty=2,lwd=2)
+lines(y=lam.med.all.lis[Time:(Time+Time.fore-1)],
+      x=seq(Time,(Time+Time.fore-1)),lty=2,lwd=2)
+lines(y=lam.med.all.noin[Time:(Time+Time.fore-1)],
+      x=seq(Time,(Time+Time.fore-1)),lty=2,lwd=2)
+
 
 abline(h=1)
 
 legend("topright", 
-       legend=c("Current Management", "All In Situ PAIS"), pch=c(16,16),
+       legend=c("SSP2-4.5, 0.5 m", "SSP5-8.5, 2 m"), pch=c(16,16),
        bty="n",col=c(col.plot[3],col.plot[5]),
        x.intersp=0.7, y.intersp=1.0, cex=1.1, pt.cex=1.5)
 
